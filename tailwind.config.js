5c359bc701e976e7bfa1f7ebb753297b8217752c
Mohammad Javad Rezaei <mjr0924@gmail.com>
2023-08-22 14:25:28 +0330

    config setting App

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      fontFamily: {
-        pop: ['"Poppins"', ...defaultTheme.fontFamily.sans]
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
+
