67acc793de292ce70d53014cfb9d3c76944b8678
Mohammad Javad Rezaei <mjr0924@gmail.com>
2023-08-26 21:54:54 +0330

    styled header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors:{
-      'bg-color':'#fdfdfd',
-      'text-color':'#333',
-      'main-color':'#754ef9',
-      'white-color':'#fdfdfd',
-      'shadow-color':'rgba(0,0,0,.2)'
+    extend: {
+      colors:{
+        'bg-color':'#fdfdfd',
+        'text-color':'#333',
+        'main-color':'#754ef9',
+        'white-color':'#fdfdfd',
+        'shadow-color':'rgba(0,0,0,.2)'
+      },
     },
-    extend: {},
    
   },
   plugins: [],
